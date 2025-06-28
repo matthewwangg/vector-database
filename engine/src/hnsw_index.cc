11fc2db0d6fe5ee823429b9c7e8caf17111f9718
Matt Wang <matthewianwang@gmail.com>
2025-06-27 21:49:27 -0400

    feat(hnsw): add level search function

--- engine/src/hnsw_index.cc
+++ engine/src/hnsw_index.cc
@@ -1,5 +1,6 @@
 #include "hnsw_index.h"
 
+#include <queue>
 #include <random>
 
 namespace vector_db_engine {
@@ -27,7 +28,52 @@ std::vector<Id> HNSWIndex::Search(const Vector& query, std::size_t k, std::size_
 }
 
 std::vector<Id> HNSWIndex::SearchLevel(const Vector& query, std::optional<Id> entry_point, std::size_t ef, int level) const {
-    return {};
+    if (!entry_point.has_value() || ef <= 0) {
+        return {};
+    }
+
+    std::priority_queue<std::pair<float, Id>, std::vector<std::pair<float, Id>>, std::greater<>> candidates;
+    std::priority_queue<std::pair<float, Id>> top_ef;
+    std::unordered_set<Id> visited;
+
+    Id entry_point_id = entry_point.value();
+    float entry_point_distance = ComputeDistance(query, nodes_.at(entry_point_id).vector);
+
+    candidates.emplace(entry_point_distance, entry_point_id);
+    top_ef.emplace(entry_point_distance, entry_point_id);
+    visited.insert(entry_point_id);
+
+    while (!candidates.empty()) {
+        auto [distance, node_id] = candidates.top();
+        candidates.pop();
+        visited.insert(node_id);
+
+        if (top_ef.size() == ef) {
+            if (distance > top_ef.top().first) {
+                break;
+            }
+            top_ef.pop();
+        }
+
+        top_ef.emplace(std::pair<float, Id>(distance, node_id));
+
+        for (Id neighbor : nodes_.at(node_id).neighbors.at(level)) {
+            if (visited.contains(neighbor)) {
+                continue;
+            }
+            float neighbor_distance = ComputeDistance(query, nodes_.at(neighbor).vector);
+            candidates.emplace(std::pair<float, Id>(neighbor_distance, neighbor));
+        }
+    }
+
+    std::vector<Id> nearest;
+
+    while (!top_ef.empty()) {
+        nearest.push_back(top_ef.top().second);
+        top_ef.pop();
+    }
+
+    return nearest;
 }
 
 float HNSWIndex::ComputeDistance(const vector_db_engine::Vector& a, const vector_db_engine::Vector& b) const {
