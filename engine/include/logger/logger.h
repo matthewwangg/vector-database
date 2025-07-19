cfc7ec760885e134abbbd253b829df0f32253e69
Matt Wang <matthewianwang@gmail.com>
2025-07-18 22:14:54 -0400

    feat(logger); add logger interface

---
+++ engine/include/logger/logger.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR_DATABASE_LOGGER_H
+#define VECTOR_DATABASE_LOGGER_H
+
+#include <string>
+
+class Logger {
+public:
+    virtual ~Logger() = default;
+
+    virtual void Info(const std::string& message);
+    virtual void Warn(const std::string& message);
+    virtual void Error(const std::string& message);
+};
+
+#endif //VECTOR_DATABASE_LOGGER_H
