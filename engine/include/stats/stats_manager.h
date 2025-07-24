3f8536dc182948d405cdfb6a1bf027bd4cb1a9ce
Matt Wang <matthewianwang@gmail.com>
2025-07-24 19:15:25 -0400

    feat(stats): add base stats manager

--- engine/include/stats/stats_manager.h
+++ engine/include/stats/stats_manager.h
@@ -1,4 +1,43 @@
 #ifndef VECTOR_DATABASE_STATS_MANAGER_H
 #define VECTOR_DATABASE_STATS_MANAGER_H
 
+#include <atomic>
+#include <cstdint>
+#include <shared_mutex>
+
+namespace vector_db_engine {
+
+class StatsManager {
+public:
+    struct Stats {
+        uint64_t vector_count = 0;
+        uint64_t deleted_count = 0;
+        uint64_t stale_count = 0;
+    };
+
+    enum class StatType {
+        VECTOR = 0,
+        DELETED = 1,
+        STALE = 2,
+    };
+
+    StatsManager();
+
+    Stats GetStats() const;
+    bool GetRemovedFlag() const;
+
+    void Increment(StatType stat_type);
+    void Reset(StatType stat_type);
+
+    void SetRemovedFlag(bool value);
+
+private:
+    Stats stats_;
+    std::atomic<bool> removed_;
+
+    mutable std::shared_mutex stats_mutex_;
+};
+
+} // namespace vector_db_engine
+
 #endif //VECTOR_DATABASE_STATS_MANAGER_H
