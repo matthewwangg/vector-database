601aca280d9da1cca0af814ab07f888aba3638af
Matt Wang <matthewianwang@gmail.com>
2025-07-28 01:33:30 -0400

    feat(index): add base flat index class

--- engine/include/index/flat_index.h
+++ engine/include/index/flat_index.h
@@ -1,4 +1,25 @@
 #ifndef VECTOR_DATABASE_FLAT_INDEX_H
 #define VECTOR_DATABASE_FLAT_INDEX_H
 
+#include <cstdint>
+#include <vector>
+
+#include "vector_index.h"
+
+namespace vector_db_engine {
+
+class FlatIndex : public VectorIndex {
+public:
+    FlatIndex();
+
+    void Insert(Id id, const Vector& vector);
+    void Remove(Id id);
+    std::vector<Id> Search(const Vector& query, std::size_t k, std::size_t search_param) const;
+
+    void Cleanup();
+    void Reindex();
+};
+
+} // namespace vector_db_engine
+
 #endif //VECTOR_DATABASE_FLAT_INDEX_H
