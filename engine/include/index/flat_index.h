74eeeb64d9a074d0720e548e7d8f8aa38fb48f66
Matt Wang <matthewianwang@gmail.com>
2025-07-28 19:38:32 -0400

    feat(index): implement flat index methods

--- engine/include/index/flat_index.h
+++ engine/include/index/flat_index.h
@@ -2,6 +2,7 @@
 #define VECTOR_DATABASE_FLAT_INDEX_H
 
 #include <cstdint>
+#include <unordered_map>
 #include <vector>
 
 #include "vector_index.h"
@@ -10,14 +11,26 @@ namespace vector_db_engine {
 
 class FlatIndex : public VectorIndex {
 public:
-    FlatIndex();
+    enum class DistanceMetric {
+        L2,
+        Cosine
+    };
 
-    void Insert(Id id, const Vector& vector);
-    void Remove(Id id);
-    std::vector<Id> Search(const Vector& query, std::size_t k, std::size_t search_param) const;
+    explicit FlatIndex(DistanceMetric metric);
 
-    void Cleanup();
-    void Reindex();
+    void Insert(Id id, const Vector& vector) override;
+    void Remove(Id id) override;
+    std::vector<Id> Search(const Vector& query, std::size_t k, std::size_t search_param) const override;
+
+    void Cleanup() override;
+    void Reindex() override;
+
+private:
+    float ComputeDistance(const Vector& a, const Vector& b) const;
+
+    std::unordered_map<Id, Vector> vectors_;
+
+    DistanceMetric metric_;
 };
 
 } // namespace vector_db_engine
