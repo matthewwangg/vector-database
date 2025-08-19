474e2d5abeda5ffd2018973da9d5618b92ba131f
Matt Wang <matthewianwang@gmail.com>
2025-08-19 19:52:06 -0400

    docs(logger): add logger comments

--- engine/include/logger/logger.h
+++ engine/include/logger/logger.h
@@ -3,13 +3,23 @@
 
 #include <string>
 
+namespace vector_db_engine {
+
+// Logger is the interface implemented by all logger types.
 class Logger {
 public:
     virtual ~Logger() = default;
 
+    // Log an INFO type message.
     virtual void Info(const std::string& message, const std::string& name) = 0;
+
+    // Log a WARN type message.
     virtual void Warn(const std::string& message, const std::string& name) = 0;
+
+    // Log an ERROR type message.
     virtual void Error(const std::string& message, const std::string& name) = 0;
 };
+
+} // namespace vector_db_engine
 
 #endif //VECTOR_DATABASE_LOGGER_H
--- engine/src/metrics/metrics_manager.cc
+++ engine/src/metrics/metrics_manager.cc
@@ -42,7 +42,7 @@ void MetricsManager::CalculateSearchLatency(float latency) {
 
     // Update the average search latency using the current average latency, the new latency, and the new search count.
     metrics_.average_search_latency_ms = ((metrics_.average_search_latency_ms * metrics_.search_count) + latency) / (metrics_.search_count + 1);
-    
+
     metrics_.max_search_latency_ms = std::max(metrics_.max_search_latency_ms, latency);
     metrics_.min_search_latency_ms = std::min(metrics_.min_search_latency_ms, latency);
 }
